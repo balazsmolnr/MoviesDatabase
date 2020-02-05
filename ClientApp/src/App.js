8e7e11c92a2d95f209be4b62741c27dee1343d37
balazsmolnr <balazsmolnr@gmail.com>
2020-02-05 13:27:39 +0100

    fetch data and display details

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,102 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import './style/App.css';
+import MovieRow from './MovieCard';
+import apiConfig from './ApiKeys';
 
-import './custom.css'
+class App extends Component {
+    constructor(props) {
 
-export default class App extends Component {
-  static displayName = App.name;
+        super(props);
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+        this.state = {};
+
+        var inputField = document.getElementsByClassName("inputField").value;
+
+        if (inputField !== '') {
+            this.getSearchTerm();
+        }
+
+        this.getTopRatedMovies();
+
+    }
+
+    getSearchTerm = async (searchTerm) => {
+        const urlString = `https://api.themoviedb.org/3/search/movie?api_key=${apiConfig.tmdbKey}&language=en-US&page=1&query=` + searchTerm;
+
+        const response = await fetch(urlString);
+        const jsonResponse = await response.json();
+        const results = jsonResponse.results;
+        console.log(results);
+
+        var movieRows = [];
+
+        if (searchTerm) {
+            results.forEach(movie => {
+                movie.poster_src = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
+                movie.background_img = "http://image.tmdb.org/t/p/w185" + movie.backdrop_path;
+                const movieRow = <MovieRow key={movie.id} movie={movie} />
+                movieRows.push(movieRow);
+            });
+            this.setState({
+                rows: movieRows
+            })
+        }
+
+    }
+
+
+    getTopRatedMovies = async () => {
+        const urlString = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiConfig.tmdbKey}&language=en-US&page=1`
+
+
+        const response = await fetch(urlString);
+        const jsonResponse = await response.json();
+        const results = jsonResponse.results;
+        console.log(results);
+
+        var movieRows = []
+
+        results.forEach(movie => {
+            movie.poster_src = "http://image.tmdb.org/t/p/w185" + movie.poster_path
+            const movieRow = <MovieRow key={movie.id} movie={movie} />
+            movieRows.push(movieRow);
+        });
+
+        this.setState({
+            rows: movieRows
+        })
+    }
+
+    changeHandler(event) {
+        const searchTerm = event.target.value;
+        this.getSearchTerm(searchTerm);
+    }
+
+    render() {
+        return (
+            <div>
+                <table className="titleBar">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img alt="app_icon" src="https://img.icons8.com/cotton/64/000000/cinema-.png"></img>
+                            </td>
+                            <td witdh="8">
+                                <h1>MoviesDB</h1>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="searchBar">
+                    <input onChange={this.changeHandler.bind(this)} className="inputField" placeholder="What are you looking for?"></input>
+                </div>
+                <div>
+                    {this.state.rows}
+                </div>
+
+            </div>
+        );
+    }
 }
+
+export default App;
---
+++ ClientApp/src/MovieCard.js
@@ -0,0 +1,39 @@
+﻿import React from 'react';
+
+class MovieRow extends React.Component {
+    viewMovie() {
+        const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+        window.location.href = url;
+    }
+
+    render() {
+        return (
+            <div className="movie_card" key={this.props.movie.id} id="bright">
+                <div className="info_section">
+                    <div className="movie_header">
+                        <img className="locandina" alt="poster" src={this.props.movie.poster_src} />
+                        <h1>{this.props.movie.title}</h1>
+                        {/* <h4 className="release_date">{this.props.movie.release_date}</h4> */}
+                        <span className="rate">{this.props.movie.vote_average}</span>
+                        <p className="type">{this.props.movie.release_date}</p>
+                    </div>
+                    <div className="movie_desc">
+                        <p className="text">
+                            {this.props.movie.overview}
+                        </p>
+                    </div>
+                    <div className="movie_social">
+                        <ul>
+                            <li><i className="material-icons">star</i></li>
+                            <li><i className="material-icons">share</i></li>
+                            <li><i className="material-icons">thumb_up_alt</i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="blur_back bright_back"></div>
+            </div>
+        )
+    }
+}
+
+export default MovieRow;
